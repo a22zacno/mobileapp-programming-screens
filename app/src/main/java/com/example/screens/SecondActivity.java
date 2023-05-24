ac2033b20a2aafa5d1db49fc1a0c8810755db467
a22zacno <a22zano@studnet.his.se>
2023-05-24 11:48:59 +0200

    Moved bundle extras = getIntent().getExtras and following cod to their correct placement in SecondActivity.java Removed the coloured background and layout in activity_second.xml and replaced it with a textview widget. added textView.setText(activityTxt); in order to update the textview widget in activity_second.xml

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        String activityTxt = null;
+        if (extras != null) {
+            activityTxt = extras.getString("activityTxt");
+        }
+
+        TextView textView = findViewById(R.id.activityView);
+        textView.setText(activityTxt);
     }
 }
